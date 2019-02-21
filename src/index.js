b727e05333c10da04855e7136f3be712753cdd92
Vladislav <plusch321@mail.ru>
2019-02-21 07:55:28 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let open = bracketsConfig.map((elem) => elem[0]);
+  let close = bracketsConfig.map((elem) => elem[1]);
+  let arr = str.split('');
+
+  for (let i = 0; i < arr.length; i++) {
+      // если есть закрывающая скобка из массивов;
+      if(close.includes(arr[i])) { 
+          let n = i - 1;
+          while(arr[n] == 0) n--;
+          if(arr[n] == open[close.indexOf(arr[i])]) {
+            arr[n] = 0;
+            arr[i] = 0;
+          }
+      }
+  }
+  return arr.every((elem) => elem == 0);
 }
